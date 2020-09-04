683974c37ed373fd6e5f8f7b5bf3e60a966e6cef
unknown <richardct8@gmail.com>
2020-09-04 12:34:16 -0500

    index wifi

--- ArduinoChatBot_Nodejs/index.js
+++ ArduinoChatBot_Nodejs/index.js
@@ -1,39 +1,64 @@
+
 const TelegramBot = require('node-telegram-bot-api');
-
+var mqtt = require('mqtt')
 const token = '1093970624:AAGv5-k6E53uApfVZYPwalgcE73Z5E14Ud8';
-
+//const token = '1093970624:AAGv5-k6E53uApfVZYPwalgcE73Z5E14Ud8';
 const bot = new TelegramBot(token, {
   polling: true
 });
 var IdMiChat = 1189408516;
+//var SerialPort = require('serialport');
+//var MiPuerto = new SerialPort('COM4', {
+  //baudRate: 9600,
+  //autoOpen: true
+var client = mqtt.connect('mqtt://housefox:123andres@broker.shiftr.io')
 
-var SerialPort = require('serialport');
-var MiPuerto = new SerialPort('COM3', {
-  baudRate: 9600,
-  autoOpen: true
-});
+bot.on('message', (msg) =>
 
-bot.on('message', (msg) => {
+  {
   const chatId = msg.chat.id;
   console.log("El ID del char" + chatId);
-  var Mensaje = msg.text;
-  if (Mensaje == "Encender") {
-    console.log("encendiendo el led");
-    bot.sendMessage(chatId, 'Encendere el led');
-    MiPuerto.write("H");
-  } else if (Mensaje == "Apagar") {
-    console.log("apagar el led");
-    bot.sendMessage(chatId, 'Apagare el led');
-    MiPuerto.write("L");
+  var Mensaje = msg.text.toString().toLocaleLowerCase();
+
+  if (Mensaje.indexOf("habitacion") >= 0)
+
+  {
+    console.log("encendiendo la habitacion");
+     bot.sendMessage(chatId, 'Encendere la habitacion');
+    client.publish('/fH/habitacion/casa', '1')
+
+  } else if (Mensaje.indexOf("apagar") >= 0)
+  {
+    console.log("Apagar la habitacion");
+    bot.sendMessage(chatId, 'Apagare la habitacion');
+    client.publish('/fH/habitacion/casa', '0')
   }
+
+
+  if (Mensaje.indexOf("sala") >= 0)
+
+  {
+    console.log("encendiendo la sala");
+     bot.sendMessage(chatId, 'Encendere la sala');
+    client.publish('/fs/sala/casa', '1')
+
+  } else if (Mensaje.indexOf("off") >= 0)
+  {
+    console.log("Apagar la sala");
+    bot.sendMessage(chatId, 'Apagare la sala');
+    client.publish('/fs/sala/casa', '0')
+}
+{
+  bot.sendMessage(chatId, 'Recibí tu mensaje');
+}
 });
 
-MiPuerto.setEncoding('utf8');
+//MiPuerto.setEncoding('utf8');
 
-MiPuerto.on('data', function(data) {
-  console.log("Lo que entro es " + data);
-  if (data[0] == 'H') {
-    console.log("Boton Precionado");
-    bot.sendMessage(IdMiChat, "Se preciono el boton");
-  }
-});
+//MiPuerto.on('data', function(data) {
+  //console.log("Lo que entro es " + data);
+  //if (data[0] == 'H') {
+    //console.log("Boton Precionado");
+  //  bot.sendMessage(IdMiChat, "Se preciono el boton");
+//  }
+//});
