4f9058873b5e91c13c5d8116cb66c4fc5f1e0a12
unknown <richardct8@gmail.com>
2020-09-12 23:23:52 -0500

    22

--- ArduinoChatBot_Arduino/ChatBotIOT/ChatBotIOT.ino
+++ ArduinoChatBot_Arduino/ChatBotIOT/ChatBotIOT.ino
@@ -34,15 +34,15 @@ void connect() {
     delay(100);
     Serial.print(".");
   }
-//{
-  //Serial.print("\nConectando con MQTT...");
-  //while (!client.connect("dell_house", "laptdell", "123andress")) {
-    //delay(1000);
-    //Serial.print("*");
-  //}
-  //Serial.println("\nConectado :D/ !");
-  //client.subscribe("/fh/Foco/casa");
-  //}
+{
+  Serial.print("\nConectando con MQTT...");
+  while (!client.connect("dell_house2", "laptdell", "123andress")) {
+    delay(1000);
+   Serial.print("*");
+  }
+  Serial.println("\nConectado :D/ !");
+  client.subscribe("/fh/Foco/casa");
+  }
   {
 Serial.print("\nConectando con MQTT...");
   while (!client.connect("dell_house", "housefox", "123andres")) {
@@ -51,7 +51,7 @@ Serial.print("\nConectando con MQTT...");
   }
   Serial.println("\nConectado :D !");
   client.subscribe("/dll/Foco/casa");
-  client.subscribe("/fh/Foco/casa");
+  
 
 }}
 
